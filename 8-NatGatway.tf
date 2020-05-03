acaf33139d511710d587ae8268027ff0e62a5830
EC2 Default User <ec2-user@ip-172-31-28-39.eu-west-2.compute.internal>
2020-05-03 12:52:46 +0000

    added nat gateway and routetabla

--- 8-NatGatway.tf
+++ 8-NatGatway.tf
@@ -1,9 +1,9 @@
-    # resource "aws_nat_gateway" "nat" {
-    #     allocation_id = "${aws_eip.nat.id}"
-    #     subnet_id = "${aws_subnet.public1.id}"
+    resource "aws_nat_gateway" "nat" {
+        allocation_id = "${aws_eip.nat.id}"
+        subnet_id = "${aws_subnet.public1.id}"
 
   
-# }
+ }
 # need to allocate with elastic Ip address
 resource "aws_eip" "nat" {
   # instance ="${aws_instance.web.id}"
