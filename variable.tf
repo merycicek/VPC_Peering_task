0bd2838d1e06b28653240057084a7f56aa4d5e3d
EC2 Default User <ec2-user@ip-172-31-28-39.eu-west-2.compute.internal>
2020-05-03 12:03:16 +0000

    put commands

--- variable.tf
+++ variable.tf
@@ -1,21 +1,21 @@
-variable "vpc_cidr" {}
+# variable "vpc_cidr" {}
 
-variable "private_cidr1" {}
-variable "private_cidr2" {}
-variable "private_cidr3" {}
-
-
-variable "public_cidr1" {}
-variable "public_cidr2" {}
-variable "public_cidr3" {}
+# variable "private_cidr1" {}
+# variable "private_cidr2" {}
+# variable "private_cidr3" {}
 
 
-variable "region" {}
-variable "region_name" {}
-variable "az1" {}
-variable "az2" {}
-variable "az3" {}
+# variable "public_cidr1" {}
+# variable "public_cidr2" {}
+# variable "public_cidr3" {}
+
+
+# variable "region" {}
+# variable "region_name" {}
+# variable "az1" {}
+# variable "az2" {}
+# variable "az3" {}
   
-  variable "tags" {
-    type = "map"
-}
+#   variable "tags" {
+#     type = "map"
+# }
