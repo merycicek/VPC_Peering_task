f68fddfe980982f870aeaf2a14a378d41208a82f
EC2 Default User <ec2-user@ip-172-31-28-39.eu-west-2.compute.internal>
2020-05-03 16:47:30 +0000

    ...

--- peering.tf
+++ peering.tf
@@ -58,3 +58,64 @@
 #     allow_remote_vpc_dns_resolution = true
 #   }
 # }
+#owner profile
+# provider "aws" {
+#   profile = "${var.owner_profile}"
+#   region  = "${var.region}"
+# }
+#accepter profile 
+provider "aws" {
+  alias = "accepter"
+  region  = "${var.region}"
+  profile = "${var.accepter_profile}"
+}
+#this code extract the account-id
+data "aws_vpc" "accepter" {
+    provider = "aws.accepter"
+    id = "${var.accepter_vpc_id}"
+}
+locals {
+  accepter_account_id = "${element(split(":", data.aws_vpc.accepter.arn), 4)}"
+}
+#here start to peering and acceptance 
+resource "aws_vpc_peering_connection" "owner" {
+  vpc_id = "${var.owner_vpc_id}"
+  peer_vpc_id   = "${data.aws_vpc.accepter.id}"
+  peer_owner_id = "${local.accepter_account_id}"  
+  tags {
+    Name = "peer_to_${var.accepter_profile}"
+  }
+}
+#here start to peering acceptance 
+resource "aws_vpc_peering_connection_accepter" "accepter" {
+  provider                  = "aws.accepter"
+  vpc_peering_connection_id = "${aws_vpc_peering_connection.owner.id}"
+  auto_accept               = true
+  tags {
+    Name = "peer_to_${var.owner_profile}"
+  }
+}
+#update route tables information using vpc cidr
+data "aws_vpc" "owner" {
+    id = "${var.owner_vpc_id}"
+}
+data "aws_route_tables" "accepter" {
+  provider = "aws.accepter"
+  vpc_id = "${data.aws_vpc.accepter.id}"
+}
+data "aws_route_tables" "owner" {
+  vpc_id = "${var.owner_vpc_id}"
+}
+resource "aws_route" "owner" {
+  count = "${length(data.aws_route_tables.owner.ids)}"
+  route_table_id            = "${data.aws_route_tables.owner.ids[count.index]}"
+  destination_cidr_block    = "${data.aws_vpc.accepter.cidr_block}"
+  vpc_peering_connection_id = "${aws_vpc_peering_connection.owner.id}"
+}   
+resource "aws_route" "accepter" {
+  provider = "aws.accepter"
+  count = "${length(data.aws_route_tables.accepter.ids)}"
+  route_table_id            = "${data.aws_route_tables.accepter.ids[count.index]}"
+  destination_cidr_block    = "${data.aws_vpc.owner.cidr_block}"
+  vpc_peering_connection_id = "${aws_vpc_peering_connection.owner.id}"
+} 
