9d61cd97b2d20d734f87e51e3ddc8dd35ec70dbb
EC2 Default User <ec2-user@ip-172-31-28-39.eu-west-2.compute.internal>
2020-05-03 14:01:29 +0000

    added DB secgroup file

---
+++ 15-Db_secgroup.tf
@@ -0,0 +1,32 @@
+resource "aws_security_group" "db_sec" {
+  name        = "DB"
+  description = "Allow TLS inbound traffic"
+
+  vpc_id = "${aws_vpc.myperfect_vpc.id}" 
+  tags = "${var.tags}"
+ 
+  
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ingress {
+    description = "TLS from VPC"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
