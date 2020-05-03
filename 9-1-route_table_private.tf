f2352ca5aaf0b1c47a4d41085754c40d70b1cca7
EC2 Default User <ec2-user@ip-172-31-28-39.eu-west-2.compute.internal>
2020-05-03 14:22:27 +0000

    added new route table

--- 9-1-route_table_private.tf
+++ 9-1-route_table_private.tf
@@ -10,16 +10,16 @@ resource "aws_route_table" "pr" {
 } 
 
 resource "aws_route_table_association" "p1" { 
-  subnet_id = "${aws_subnet.dev_private1.id}" 
+  subnet_id = "${aws_subnet.private1.id}" 
   route_table_id = "${aws_route_table.pr.id}" 
 } 
 
 resource "aws_route_table_association" "p2" { 
-  subnet_id = "${aws_subnet.dev_private2.id}" 
+  subnet_id = "${aws_subnet.private2.id}" 
   route_table_id = "${aws_route_table.pr.id}" 
 } 
 
 resource "aws_route_table_association" "p3" { 
-  subnet_id = "${aws_subnet.dev_private3.id}" 
+  subnet_id = "${aws_subnet.private3.id}" 
   route_table_id = "${aws_route_table.pr.id}" 
 } 
