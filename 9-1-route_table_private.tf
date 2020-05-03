9af3b221930e6d1bf00741b2e9e6d307e2375b5d
EC2 Default User <ec2-user@ip-172-31-28-39.eu-west-2.compute.internal>
2020-05-03 14:18:26 +0000

    added new file pr_route table

---
+++ 9-1-route_table_private.tf
@@ -0,0 +1,25 @@
+resource "aws_route_table" "pr" { 
+  vpc_id = "${aws_vpc.myperfect_vpc.id}" 
+  tags = "${var.tags}"
+  route { 
+    cidr_block = "0.0.0.0/0" 
+    gateway_id = "${aws_nat_gateway.nat.id}" 
+    
+} 
+ 
+} 
+
+resource "aws_route_table_association" "p1" { 
+  subnet_id = "${aws_subnet.dev_private1.id}" 
+  route_table_id = "${aws_route_table.pr.id}" 
+} 
+
+resource "aws_route_table_association" "p2" { 
+  subnet_id = "${aws_subnet.dev_private2.id}" 
+  route_table_id = "${aws_route_table.pr.id}" 
+} 
+
+resource "aws_route_table_association" "p3" { 
+  subnet_id = "${aws_subnet.dev_private3.id}" 
+  route_table_id = "${aws_route_table.pr.id}" 
+} 
