7de68a5897d26f5dd6f96801bbaac6e4ef2b6f1b
EC2 Default User <ec2-user@ip-172-31-28-39.eu-west-2.compute.internal>
2020-05-03 12:05:19 +0000

    ...

--- variable.tf
+++ variable.tf
@@ -10,8 +10,8 @@
 # variable "public_cidr3" {}
 
 
-# variable "region" {}
-# variable "region_name" {}
+variable "region" {}
+ variable "region_name" {}
 # variable "az1" {}
 # variable "az2" {}
 # variable "az3" {}
