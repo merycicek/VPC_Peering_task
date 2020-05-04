a09fa8ce6f0ae16e92258004a8eff819458e53d8
EC2 Default User <ec2-user@ip-172-31-28-39.eu-west-2.compute.internal>
2020-05-04 02:05:37 +0000

    ...

--- reguester_peering_route_table.tf
+++ reguester_peering_route_table.tf
@@ -1,11 +1,11 @@
-resource "aws_route" "r"{
-    route_table_id  ="${aws_route_table.route_table2.id}"
+resource "aws_route" "rp"{
+    route_table_id  ="${aws_route_table.r.id}"
     destination_cidr_block ="${var.vpc_cidr}" 
     vpc_peering_connection_id ="${aws_vpc_peering_connection.peer.id}"
 }
 
-resource "aws_route" "r1"{
-    route_table_id  ="${aws_route_table.route_table_private__new.id}"
+resource "aws_route" "rp1"{
+    route_table_id  ="${aws_route_table.pr.id}"
     destination_cidr_block ="${var.vpc_cidr}" 
     vpc_peering_connection_id ="${aws_vpc_peering_connection.peer.id}"
 }
