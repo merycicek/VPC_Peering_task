5413bd384f7fcb579400cbb191c17feda761944b
merycicek <63379264+merycicek@users.noreply.github.com>
2020-05-04 05:23:51 -0500

    Update reguester_peering_route_table.tf

--- reguester_peering_route_table.tf
+++ reguester_peering_route_table.tf
@@ -9,3 +9,8 @@ resource "aws_route" "rp1"{
     destination_cidr_block ="${var.vpc_cidr}" 
     vpc_peering_connection_id ="${aws_vpc_peering_connection.peer.id}"
 }
+
+# there are 2 peering route table which use route table id'ies 
+
+                                  1- public route table id
+                                  2- private route table id
